665172b1b6d4cde325def7d8fecd794edab695e4
silvermaking <silvermaking@gmail.com>
2022-03-14 17:45:44 +0900

    back conflict 해결

--- back/accounts/models.py
+++ back/accounts/models.py
@@ -4,15 +4,9 @@ from movies.models import Genre
 
 
 class User(AbstractUser):
-<<<<<<< Updated upstream
-    picture = models.CharField(max_length=255, null=True)
-    category_list = models.ManyToManyField(Genre, blank=True)
-    birthDate = models.DateField(null=True)
-=======
     picture = models.CharField(max_length=255, null=True, blank=True)
     category_list = models.ManyToManyField(Genre)
     birthDate = models.DateField(null=True,blank=True)
->>>>>>> Stashed changes
     gender = models.BooleanField(default=True)
     oauth_type = models.CharField(max_length=255, null=True,blank=True)
     username2 = models.CharField(max_length=255,null=True,blank=True)
