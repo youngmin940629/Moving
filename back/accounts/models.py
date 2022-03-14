2da950f47bfeef1557ad3ea68803c6fece0d17f6
youngmin <kgjdudals@naver.com>
2022-03-14 17:58:04 +0900

    디렉토리 정리

--- back/accounts/models.py
+++ back/accounts/models.py
@@ -5,7 +5,7 @@ from movies.models import Genre
 
 class User(AbstractUser):
     picture = models.CharField(max_length=255, null=True, blank=True)
-    category_list = models.ManyToManyField(Genre)
+    category_list = models.ManyToManyField(Genre, blank=True)
     birthDate = models.DateField(null=True,blank=True)
     gender = models.BooleanField(default=True)
     oauth_type = models.CharField(max_length=255, null=True,blank=True)
