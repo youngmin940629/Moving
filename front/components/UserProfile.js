be264fb25a3ae46577ca5f4a9da27899fbd4cf8d
swchae516 <swchae516@gmail.com>
2022-03-15 18:00:01 +0900

    회원가입 & 프로필

--- front/components/UserProfile.js
+++ front/components/UserProfile.js
@@ -12,7 +12,10 @@ const InputStyle = styled(Input)`
 const UserProfile = () => {
   const [token, setToken] = useState(null);
   const [user, setUser] = useState(null);
-  const [userInfo, setUserInfo] = useState(null);
+  const [userInfo, setUserInfo] = useState({
+    username: '',
+  });
+  const [category, setCategory] = useState(null);
   useEffect(() => {
     setToken(localStorage.getItem('JWT token'));
   }, []);
@@ -27,7 +30,13 @@ const UserProfile = () => {
         .get(`http://localhost:8000/accounts/${user.user_id}`)
         .then(res => {
           console.log(res.data);
-          setUserInfo(res.data);
+          setCategory(res.data.category_list);
+          setUserInfo({
+            username: res.data.username,
+            username2: res.data.username2,
+            birthDate: res.data.birthDate,
+            gender: res.data.gender,
+          });
         })
         .catch(err => {
           console.log(err);
@@ -36,12 +45,17 @@ const UserProfile = () => {
   }, [user]);
   return (
     <>
-      {userInfo != null && (
+      {userInfo != null && category != null && (
         <div>
           <div>이메일 : {userInfo.username}</div>
           <div>닉네임: {userInfo.username2}</div>
           <div>성별: {userInfo.gender ? '남' : '여'}</div>
-          <div>장르: {userInfo.category_list}</div>
+          <div>
+            장르:
+            {category.map((item, idx) => {
+              return <span key={idx}>{item.name}</span>;
+            })}
+          </div>
           <div>생년월일: {userInfo.birthDate}</div>
         </div>
       )}
