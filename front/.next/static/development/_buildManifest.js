482ce7c27729575c5a1dd2c978e29c8f1d309f01
정재현 <jungjh122@naver.com>
2022-03-15 09:04:21 +0000

    next 삭제

--- front/.next/static/development/_buildManifest.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< Updated upstream
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/login":["static\u002Fchunks\u002Fpages\u002Flogin.js"],"/profile":["static\u002Fchunks\u002Fpages\u002Fprofile.js"],sortedPages:["\u002F_app","\u002F_error","\u002Flogin","\u002Fprofile"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
-=======
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/board/[id]":["static\u002Fchunks\u002Fpages\u002Fboard\u002F[id].js"],"/community":["static\u002Fchunks\u002Fpages\u002Fcommunity.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fboard\u002F[id]","\u002Fcommunity"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
->>>>>>> Stashed changes
