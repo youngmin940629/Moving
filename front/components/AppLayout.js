3c8fca35c8bda768c5bff271325d572d11d209c7
문찬송 <moon.sw.good@gmail.com>
2022-03-16 18:06:42 +0900

    상단바 user관련 드롭다운메뉴 추가

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,10 +1,13 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
-import { Layout, Menu, Input, Button } from 'antd';
+import { Layout, Menu, Input, Button, Dropdown, message } from 'antd';
 import styled from 'styled-components';
 import { useSelector, useDispatch } from 'react-redux';
 import { LOAD_USER_REQUEST, logoutRequestAction } from '../reducers/user';
+import { DownOutlined, UserOutlined, HeartOutlined, LogoutOutlined } from '@ant-design/icons';
+import jwt_decode from 'jwt-decode';
+import Router from 'next/router';
 
 const { Header, Content, Footer } = Layout;
 
@@ -43,7 +46,46 @@ const MovingLogo = styled.img`
   cursor: pointer;
 `;
 
+const UserButton = styled(Button)`
+.ant-click-animating-node {
+  display: none;
+}
+`
+
+
 const AppLayout = ({ children }) => {
+  const logout = () => {
+    console.log(dispatch(logoutRequestAction()));
+    Router.push("/")
+  }
+
+  const menu = (
+    <Menu>
+      <Menu.Item key="7" icon={<UserOutlined />} onClick={() => {Router.push("/profile")}}>
+        프로필
+      </Menu.Item>
+      <Menu.Item key="8" icon={<HeartOutlined />}>
+        좋아요한 영화
+      </Menu.Item>
+      <Menu.Item key="9" icon={<LogoutOutlined />} onClick={logout}>
+        로그아웃
+      </Menu.Item>
+    </Menu>
+  );
+
+  const [user, setUser] = useState('');
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    setToken(localStorage.getItem('JWT token'));
+  }, []);
+  useEffect(() => {
+    if (token) {
+      setUser(jwt_decode(token));
+      console.log(jwt_decode(token))
+    }
+  }, [token]);
+
   const dispatch = useDispatch();
   const { me } = useSelector(state => state.user);
   useEffect(() => {
@@ -59,10 +101,11 @@ const AppLayout = ({ children }) => {
           <Link href="/">
             <MovingLogo src="/img/logo-colored.png" />
           </Link>
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['3']}>
+          {/* <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['3']}> */}
+          <Menu theme="dark" mode="horizontal">
             {me !== null ? (
               <>
-                <Menu.Item key="0">
+                {/* <Menu.Item key="0">
                   <Link href="/">
                     <a
                       onClick={() => {
@@ -77,35 +120,45 @@ const AppLayout = ({ children }) => {
                   <Link href="/profile">
                     <a>회원정보</a>
                   </Link>
-                </Menu.Item>
-                <Menu.Item key="2">
+                </Menu.Item> */}
+                <Menu.Item key="0">
                   <Link href="/community">
                     <a>커뮤니티</a>
                   </Link>
+                </Menu.Item>
+                <Menu.Item key="1">
+                  <SearchInput enterButton placeholder='영화 검색'/>
+                </Menu.Item>
+                <Menu.Item key="2">
+                  <Dropdown overlay={menu}>
+                    <Button style={{background:'#2CD4AC', color:'white'}}>
+                      { user.username } <UserOutlined />
+                    </Button>
+                  </Dropdown>
                 </Menu.Item>
               </>
             ) : (
               <>
-                <Menu.Item key="0">
+                <Menu.Item key="3">
                   <Link href="/login">
                     <a>로그인</a>
                   </Link>
                 </Menu.Item>
-                <Menu.Item key="1">
+                <Menu.Item key="4">
                   <Link href="/signup">
                     <a>회원가입</a>
                   </Link>
                 </Menu.Item>
-                <Menu.Item key="2">
+                <Menu.Item key="5">
                   <Link href="/community">
                     <a>커뮤니티</a>
                   </Link>
                 </Menu.Item>
+                <Menu.Item key="6">
+                  <SearchInput enterButton placeholder='영화 검색'/>
+                </Menu.Item>
               </>
             )}
-            <Menu.Item key="5">
-              <SearchInput enterButton placeholder="영화 검색" />
-            </Menu.Item>
           </Menu>
         </Header>
         <Content style={{ padding: '0 50px' }}>
