9792af0f6fc4155b5648e5eddab63d9abc4818f2
문찬송 <moon.sw.good@gmail.com>
2022-03-17 16:39:43 +0900

    메인 캐러셀 hover shadow 추가

--- front/components/MainCarousel.js
+++ front/components/MainCarousel.js
@@ -5,6 +5,13 @@ import Router from 'next/router';
 import axios from 'axios';
 import styled from 'styled-components';
 
+const StyledImg = styled.img`
+  cursor: pointer;
+  &:hover {
+    border-color: transparent;
+    box-shadow: 0 1px 2px -2px rgb(0 0 0 / 16%), 0 3px 6px 0 rgb(0 0 0 / 12%), 0 5px 12px 4px rgb(0 0 0 / 9%);
+  }
+`;
 
 const MainCarousel = () => {
   const [movies, setMovies] = useState([]);
@@ -55,11 +62,10 @@ const MainCarousel = () => {
             className={idx === imageIndex ? 'slide activeSlide' : 'slide'}
             key={idx}
           >
-            <img 
+            <StyledImg 
               src={movie.poster_path} 
               alt={`${movie.title} poster image`} 
               onClick={() => Router.push(`/movie/${movie.id}`)}
-              style={{cursor: 'pointer'}}
             />
           </div>
         ))}
