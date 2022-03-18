27d0f1676099f9b6fe5b578dfa0c1b9d924ee249
byeongjaeyu <ybj121725@gmail.com>
2022-03-18 14:43:06 +0900

    ADD getuserpk 프론트 연동

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import 'antd/dist/antd.css';
 import Head from 'next/head';
 import '../src/App.css';
 import wrapper from '../store/configureStore';
+import axios from 'axios';
 
 const App = ({ Component, pageProps }) => {
+  useEffect(()=>{
+    const token = localStorage.getItem('JWT token');
+    if(token){
+      axios({
+        url:'http://localhost:8000/accounts/getuserpk/',
+        method:'post',
+        headers:{'Authorization':`JWT ${token}`}
+      }).then(function(res){
+        sessionStorage.setItem('id',res.data.pk)
+      })
+    }
+    return ()=>{
+      let id = sessionStorage.getItem('id');
+      if(id){
+        sessionStorage.removeItem('id');
+      }
+    }
+  },[])
   return (
     <>
       <Head>
