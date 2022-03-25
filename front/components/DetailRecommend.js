fd0b167a19de0fe31197c21aa9feb4b7447c6a8d
swchae516 <swchae516@gmail.com>
2022-03-25 17:16:01 +0900

    0325

--- front/components/DetailRecommend.js
+++ front/components/DetailRecommend.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import GenreRecommend from './DetailRecommend/genre'
-import MovieMfRecommend from './DetailRecommend/MovieMf'
+import React from 'react';
+import GenreRecommend from './DetailRecommend/Genre';
+import MovieMfRecommend from './DetailRecommend/MovieMf';
 
 export default function DetailRecommend() {
   return (
     <>
-      <div className='container'>
-        <GenreRecommend/>
-        <MovieMfRecommend/>
+      <div className="container">
+        <GenreRecommend />
+        <MovieMfRecommend />
       </div>
       <style jsx>
         {`
-          .container{
-            background:white;
-            width:50%;
-            height:50%;
+          .container {
+            background: white;
+            width: 50%;
+            height: 50%;
           }
         `}
       </style>
     </>
-  )
+  );
 }
